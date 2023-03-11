bc5512d4fa0966aba4ce50eb926093fcbad5a9f4
raphi <raphi@PCHOME>
2023-03-11 14:08:21 +0100

    GetCompass

--- Kompass/ViewModels/MagnetViewModel.cs
+++ Kompass/ViewModels/MagnetViewModel.cs
@@ -3,10 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
 
 namespace Kompass.ViewModels
 {
-    public class MagnetViewModel
+    public class MagnetViewModel : ViewModelBase
     {
+
+        private double _compassRotation; 
+
+        public ICommand GetCompass { get; set; }
+
+
+        public MagnetViewModel()
+        {
+            GetCompass = new AsyncRelayCommand(ToggleCompass);
+        }
+
+        private async Task ToggleCompass()
+        {
+            if (Compass.Default.IsSupported)
+            {
+                if (!Compass.Default.IsMonitoring)
+                {
+                    // Turn on compass
+                    Compass.Default.ReadingChanged += Compass_ReadingChanged;
+                    Compass.Default.Start(SensorSpeed.UI);
+                }
+                else
+                {
+                    // Turn off compass
+                    Compass.Default.Stop();
+                    Compass.Default.ReadingChanged -= Compass_ReadingChanged;
+                }
+            }
+        }
+
+        private void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            // Update UI Label with compass state
+            var heading = e.Reading.HeadingMagneticNorth;
+            CompassRotation =  Convert.ToDouble(heading);
+        }
+
+        public double CompassRotation
+        {
+            get { return _compassRotation; }
+            set
+            {
+                SetProperty(ref _compassRotation, value);
+            }
+        }
     }
 }
--- Kompass/Views/MagnetPage.xaml.cs
+++ Kompass/Views/MagnetPage.xaml.cs
@@ -1,3 +1,4 @@
+using Kompass.ViewModels;
 using Microsoft.Maui.Devices.Sensors;
 
 namespace Kompass.Views;
@@ -5,36 +6,12 @@ namespace Kompass.Views;
 
 public partial class MagnetPage : ContentPage
 {
+
+	MagnetViewModel _magnetViewModel;
 	public MagnetPage()
 	{
+		_magnetViewModel = new MagnetViewModel();
+		BindingContext = _magnetViewModel;
 		InitializeComponent();
-        ToggleCompass();
-
-    }
-
-    private void ToggleCompass()
-    {
-        if (Compass.Default.IsSupported)
-        {
-            if (!Compass.Default.IsMonitoring)
-            {
-                // Turn on compass
-                Compass.Default.ReadingChanged += Compass_ReadingChanged;
-                Compass.Default.Start(SensorSpeed.UI);
-            }
-            else
-            {
-                // Turn off compass
-                Compass.Default.Stop();
-                Compass.Default.ReadingChanged -= Compass_ReadingChanged;
-            }
-        }
-    }
-
-    private void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
-    {
-        // Update UI Label with compass state
-        CompassLabel.TextColor = Colors.Green;
-        CompassLabel.Text = $"Compass: {e.Reading}";
     }
 }
