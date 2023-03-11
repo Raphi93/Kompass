9e6dfcdee0a8d8d41f0c075fe380a3e02e3fd99d
raphi <raphi@PCHOME>
2023-03-11 13:30:06 +0100

    Commpass

--- Kompass/Views/MagnetPage.xaml.cs
+++ Kompass/Views/MagnetPage.xaml.cs
@@ -1,4 +1,7 @@
+using Microsoft.Maui.Devices.Sensors;
+
 namespace Kompass.Views;
+
 
 public partial class MagnetPage : ContentPage
 {
@@ -6,4 +9,30 @@ public partial class MagnetPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ToggleCompass()
+    {
+        if (Compass.Default.IsSupported)
+        {
+            if (!Compass.Default.IsMonitoring)
+            {
+                // Turn on compass
+                Compass.Default.ReadingChanged += Compass_ReadingChanged;
+                Compass.Default.Start(SensorSpeed.UI);
+            }
+            else
+            {
+                // Turn off compass
+                Compass.Default.Stop();
+                Compass.Default.ReadingChanged -= Compass_ReadingChanged;
+            }
+        }
+    }
+
+    private void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+    {
+        // Update UI Label with compass state
+        CompassLabel.TextColor = Colors.Green;
+        CompassLabel.Text = $"Compass: {e.Reading}";
+    }
 }
